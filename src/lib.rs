d5e0e30a061204f0a2e09c139b83acdf8111ab24
Bo Lu <lv.patrick@gmail.com>
2019-01-09 15:10:50 +1100

    prettify log entries

--- src/lib.rs
+++ src/lib.rs
@@ -45,9 +45,36 @@ impl Config {
     }
 }
 
+/// The log styles
+struct Style {
+    lvl_trace: JsValue,
+    lvl_debug: JsValue,
+    lvl_info: JsValue,
+    lvl_warn: JsValue,
+    lvl_error: JsValue,
+    tgt: JsValue,
+    args: JsValue,
+}
+
+impl Style {
+    fn new() -> Style {
+        let base = String::from("color: white; padding: 0 3px; background:");
+        Style {
+            lvl_trace: JsValue::from_str(&(format!("{} darkgray;", base))),
+            lvl_debug: JsValue::from_str(&(format!("{} darkgreen;", base))),
+            lvl_info: JsValue::from_str(&(format!("{} green;", base))),
+            lvl_warn: JsValue::from_str(&(format!("{} gold;", base))),
+            lvl_error: JsValue::from_str(&(format!("{} red;", base))),
+            tgt: JsValue::from_str("font-weight: bold; color: inherit"),
+            args: JsValue::from_str("background: inherit; color: inherit"),
+        }
+    }
+}
+
 /// The logger
 struct WasmLogger {
     config: Config,
+    style: Style,
 }
 
 impl Log for WasmLogger {
@@ -61,18 +88,34 @@ impl Log for WasmLogger {
 
     fn log(&self, record: &Record) {
         if self.enabled(record.metadata()) {
-            let s = JsValue::from(format!(
-                "{} {}\n{}",
+            let style = &self.style;
+            let s = JsValue::from_str(&format!(
+                "[%c{: <5}%c {}%c] {}",
                 record.level(),
                 record.target(),
                 record.args()
             ));
             match record.level() {
-                Level::Trace => console::log_1(&s),
-                Level::Debug => console::debug_1(&s),
-                Level::Info => console::info_1(&s),
-                Level::Warn => console::warn_1(&s),
-                Level::Error => console::error_1(&s),
+                Level::Trace => console::log_4(&s,
+                                               &style.lvl_trace,
+                                               &style.tgt,
+                                               &style.args),
+                Level::Debug => console::debug_4(&s,
+                                                 &style.lvl_debug,
+                                                 &style.tgt,
+                                                 &style.args),
+                Level::Info => console::info_4(&s,
+                                               &style.lvl_info,
+                                               &style.tgt,
+                                               &style.args),
+                Level::Warn => console::warn_4(&s,
+                                               &style.lvl_warn,
+                                               &style.tgt,
+                                               &style.args),
+                Level::Error => console::error_4(&s,
+                                                 &style.lvl_error,
+                                                 &style.tgt,
+                                                 &style.args),
             }
         }
     }
@@ -80,10 +123,14 @@ impl Log for WasmLogger {
     fn flush(&self) {}
 }
 
+/// Since wasm doesn't support thread yet, no worry about Send and Sync for now
+unsafe impl Send for WasmLogger {}
+unsafe impl Sync for WasmLogger {}
+
 /// Initialize the logger which the given config. If failed, it will log a message to the the browser console.
 pub fn init(config: Config) {
     let max_level = config.level;
-    let wl = WasmLogger { config };
+    let wl = WasmLogger { config, style: Style::new() };
 
     match log::set_boxed_logger(Box::new(wl)) {
         Ok(_) => log::set_max_level(max_level.to_level_filter()),
