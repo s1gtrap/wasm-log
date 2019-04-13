153250ed91d8dd7ae65c5d1c3ab87ce2b8c7ea0e
Limira <2409315-limira-rs@users.noreply.gitlab.com>
2019-04-14 04:45:09 +0700

    Add example and screenshot

---
+++ examples/log/README.md
@@ -0,0 +1,5 @@
+# Example of wasm-logger
+
+Use for taking a screenshot
+
+This example is a fork of [`wasm-bindgen`/examples/without-a-bundler](https://github.com/rustwasm/wasm-bindgen/tree/master/examples/without-a-bundler).
---
+++ examples/log/src/lib.rs
@@ -0,0 +1,28 @@
+use wasm_bindgen::prelude::*;
+
+// Called when the wasm module is instantiated
+#[wasm_bindgen(start)]
+pub fn main() -> Result<(), JsValue> {
+    wasm_logger::init(wasm_logger::Config::new(log::Level::Debug));
+    // Use `web_sys`'s global `window` function to get a handle on the global
+    // window object.
+    let window = web_sys::window().expect("no global `window` exists");
+    let document = window.document().expect("should have a document on window");
+    let body = document.body().expect("document should have a body");
+
+    // Manufacture the element we're gonna append
+    let val = document.create_element("p")?;
+    val.set_inner_html("Open the browser's console to see log outputs");
+
+    body.append_child(&val)?;
+
+    log::info!("Hello, this is a log entry from Rust");
+
+    Ok(())
+}
+
+#[wasm_bindgen]
+pub fn add(a: u32, b: u32) -> u32 {
+    log::info!("Adding: {}+{}", a, b);
+    a + b
+}
